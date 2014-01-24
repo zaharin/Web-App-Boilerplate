99c5fc4c753e4861942934136a1285566920cab5
Konstantin Tarkus <hello@tarkus.me>
2014-01-25 03:42:08 +0400

    Add a static file server (Express) and LiveReload. Closes #8, #12

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 // For more information on how to configure a task runner, please visit:
 // https://github.com/gulpjs/gulp
 
-var gulp   = require('gulp');
-var clean  = require('gulp-clean');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var less   = require('gulp-less');
-var csso   = require('gulp-csso');
-var es     = require('event-stream');
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var clean       = require('gulp-clean');
+var concat      = require('gulp-concat');
+var rename      = require('gulp-rename');
+var uglify      = require('gulp-uglify');
+var less        = require('gulp-less');
+var csso        = require('gulp-csso');
+var es          = require('event-stream');
+var embedlr     = require("gulp-embedlr");
+var livereload  = require('gulp-livereload');
+var path        = require('path');
+var express     = require('express');
+var lr          = require('tiny-lr');
+var server      = lr();
+
+// Create a server for static files
+// For more information, please visit: https://github.com/visionmedia/express
+var app = express();
+app.use(express.static(path.resolve('./dist')));
+app.listen(8080, function() {
+    gutil.log('Listening on http://localhost:8080');
+});
 
 gulp.task('clean', function() {
     // Clear the destination folder
@@ -33,7 +48,8 @@ gulp.task('scripts', function () {
     return gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
         .pipe(concat('app.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(livereload(server));
 });
 
 gulp.task('styles', function () {
@@ -42,20 +58,39 @@ gulp.task('styles', function () {
         .pipe(less())
         .pipe(rename('app.css'))
         .pipe(csso())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(livereload(server));
+});
+
+// The dist task (used to store all files that will go to the server)
+gulp.task('dist', function () {
+    gulp.run('clean', 'copy', 'scripts', 'styles');
 });
 
 // The default task (called when you run `gulp`)
 gulp.task('default', function () {
-    gulp.run('clean', 'copy', 'scripts', 'styles');
 
-    // Watch .js files and run tasks if they change
-    gulp.watch('src/js/**', function () {
-        gulp.run('scripts');
-    });
+    gulp.run('dist');
 
-    // Watch .less files and run tasks if they change
-    gulp.watch('src/less/**/*.less', function () {
-        gulp.run('styles');
+    // Create a LiveReload server and watch for modifications in *.less and *.js files
+    // For more information, please visit: https://github.com/mklabs/tiny-lr
+    server.listen(35729, function (err) {
+        if (err) {
+            return console.log(err);
+        };
+
+        // Watch .js files and run tasks if they change
+        gulp.watch('src/js/**', function () {
+            gulp.run('scripts');
+        });
+
+        // Watch .less files and run tasks if they change
+        gulp.watch('src/less/**/*.less', function () {
+            gulp.run('styles');
+        });
+
+        gulp.src("./src/*.html")
+            .pipe(embedlr())
+            .pipe(gulp.dest("./dist"));
     });
 });
